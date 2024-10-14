0189b328d2a93a61c97b0e8ec2305e696b123e9d
nikitaArbuzArbuz <talker_64@vk.com>
2024-10-14 13:13:47 +0300

    service_03: fix - db structure check and default

--- src/main/resources/db/changelog/changeset/003-180413102024-create-tables.sql
+++ src/main/resources/db/changelog/changeset/003-180413102024-create-tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE t1_demo_correction.transaction_errors
 (
     id             BIGINT NOT NULL PRIMARY KEY,
-    transaction_id BIGINT,
+    transaction_id BIGINT not null unique,
     account_id     BIGINT,
     amount         DECIMAL(19, 2),
     description    VARCHAR(255),
